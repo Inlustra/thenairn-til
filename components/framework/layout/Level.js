6c9054628d1c5ac5c3d05418befbdb2113a06474
Thomas Nairn <thomas@thenairn.com>
2018-08-20 01:06:59 +0100

    Started more work on Level

--- components/framework/layout/Level.js
+++ components/framework/layout/Level.js
@@ -1,22 +1,54 @@
-import styled, { css } from 'styled-components'
-import PropTypes from 'prop-types'
+import styled, { css } from 'styled-components';
+import PropTypes from 'prop-types';
+import { media } from '../utils';
+
+const Left = styled.css`
+`
+
+const Right = styled(Left)`
+
+`
 
 export const Level = styled.div`
   display: flex;
-  align-items: "center";
-  justify-content: "space-between";
-  min-height: ${({isFullHeight}) => isFullHeight && '100vh'};
+  align-items: 'center';
+  justify-content: 'space-between';
+  min-height: ${({ isFullHeight }) => isFullHeight && '100vh'};
+
+  &:not(:last-child) {
+    margin-bottom: 1.5rem;
+  }
+
+  img {
+    display: inline-block;
+    vertical-align: top;
+  }
+
+  ${({mobile}) => mobile && css`
+    & ${Left},
+    & ${Right} {
+      display: flex;
+    }
+
+    & ${Left} + ${Right} {
+      margin-top: 0;
+    }
+  `}
+
 `;
 
 Level.Item = styled.div`
   display: flex;
   align-items: center;
-  ${({narrow}) => !narrow && css`
-    flex-grow: 1;
-    flex-shrink: 0;
-  `}
-`
+  ${({ narrow }) =>
+    !narrow &&
+    css`
+      flex-grow: 1;
+      flex-shrink: 0;
+    `} ${media('tablet')} {
+  }
+`;
 
 Level.Item.propTypes = {
   narrow: PropTypes.bool
-}
+};
