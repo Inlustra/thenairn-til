d09ee0b0cb283463dab9d7b1794d23d74711d4ea
Thomas Nairn <thomas@thenairn.com>
2019-04-02 20:22:52 +0100

    Removed bundle analyzer

--- next.config.js
+++ next.config.js
@@ -2,27 +2,10 @@ const withPlugins = require("next-compose-plugins");
 
 const withTypescript = require("@zeit/next-typescript");
 
-const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
-const withBundleAnalyzerConfig = {
-  analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
-  analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
-  bundleAnalyzerConfig: {
-    server: {
-      analyzerMode: "static",
-      reportFilename: "../bundles/server.html"
-    },
-    browser: {
-      analyzerMode: "static",
-      reportFilename: "../bundles/client.html"
-    }
-  }
-};
-
 const withImages = require("next-images");
 const withImagesConfig = {};
 
 module.exports = withPlugins([
   [withImages, withImagesConfig],
-  [withBundleAnalyzer, withBundleAnalyzerConfig],
   [withTypescript]
 ]);
