ab45fd214c03a457e1a8ee3ce8cdf39a4a76e843
Thomas Nairn <thomas@thenairn.com>
2019-10-19 18:25:49 +0100

    updated init script

---
+++ backend/init-mongo.sh
@@ -0,0 +1,22 @@
+mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+    var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
+    var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
+    var admin = db.getSiblingDB('admin');
+    admin.auth(rootUser, rootPassword);
+
+    var user = '$MONGO_USER';
+    var passwd = '$MONGO_PASSWORD';
+    var db = '$MONGO_DB';
+    db.createUser(
+    {
+        user: user,
+        pwd: passwd,
+        roles: [
+            {
+                role: "readWrite",
+                db: db
+            }
+        ]
+    }
+)
+EOF
